e004bdce2c49638ea5e7b977fc624f430a938196
Oliver Kiessler <kiessler@inceedo.com>
2012-05-03 17:13:33 +0200

    Removing unnecessary javascript und stylesheet injections

--- lib/generators/spree_dragonfly/install/install_generator.rb
+++ lib/generators/spree_dragonfly/install/install_generator.rb
@@ -31,16 +31,6 @@ code
         create_file "config/initializers/dragonfly.rb", initializer_code
       end
 
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_dragonfly\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_dragonfly\n"
-      end
-
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_dragonfly\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_dragonfly\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_dragonfly'
       end
