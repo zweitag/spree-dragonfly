c4a3db74a2b3ccb749e5aa8ee6835f8ae70f428e
Sam Figueroa <unimatrixzxero@gmail.com>
2013-02-28 10:53:22 +0100

    (WIP) slpit up tests into smaller parts

--- spec/decorators/image_decorator_spec.rb
+++ spec/decorators/image_decorator_spec.rb
@@ -2,22 +2,35 @@
 require 'spec_helper'
 
 describe "Upload a file with dragonfly" do
-  it "should create an Spree::Image file" do
-    count = Spree::Image.count
-    
-    image = Spree::Image.new
-    image.attachment = File.new("#{File.dirname(__FILE__)}/../files/trollface.jpg")
-    image.should be_valid
-    image.save.should be true
+  let(:image) do
+    Spree::Image.new.tap do |image|
+      image.attachment = File.new("#{File.dirname(__FILE__)}/../files/trollface.jpg")
+    end
+  end
 
+  it 'creates valid images' do
+    image.should be_valid
+  end
+
+  it 'can save images' do
+    image.save.should be true
+  end
+
+  it 'actually creates a image row in the database' do
+    count = Spree::Image.count
+    image.save.should be true
     Spree::Image.count.should == (count + 1)
+  end
+
+  it "should create a Spree::Image file" do
+    image.save
 
     image.id.should_not be nil
     image.attachment_uid.should_not be nil
     image.attachment_uid.include?('trollface.jpg').should be true
-    image.attachment_image_name.should == 'trollface.jpg' 
+    image.attachment_image_name.should == 'trollface.jpg'
   end
-  
+
   it "should fail with validation errors" do
     image = Spree::Image.new
     image.attachment = File.new("#{File.dirname(__FILE__)}/../files/troll-face-high-resolution.png")
